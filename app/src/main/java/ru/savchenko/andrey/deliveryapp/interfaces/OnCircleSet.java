1cf18adbf14969f2d367a35889ac110106b8247c
Andrey Savchenko <samaromku@mail.ru>
2017-09-11 14:02:54 +0300

    Add files via upload

---
+++ app/src/main/java/ru/savchenko/andrey/deliveryapp/interfaces/OnCircleSet.java
@@ -0,0 +1,11 @@
+package ru.savchenko.andrey.deliveryapp.interfaces;
+
+import android.widget.ImageView;
+
+/**
+ * Created by savchenko on 11.09.17.
+ */
+
+public interface OnCircleSet {
+    void onCircleSet(String url, ImageView imageView);
+}
